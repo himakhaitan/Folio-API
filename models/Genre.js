16f4d0ac1c908995f5885a69f760f5fdc735f644
Himanshu Khaitan <himanshukhaitan108@gmail.com>
2022-11-29 22:03:18 +0530

    Genre Model Added!

---
+++ models/Genre.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+// Defining Data Types
+let ObjectId = Schema.Types.ObjectId;
+
+// Creating a Schema
+const genreSchema = new Schema({
+  // Name of the Genre
+  name: {
+    type: String,
+    required: true,
+  },
+  // Slug of the Genre
+  slug: {
+    type: String,
+    required: true,
+  },
+  // Books of the Genre
+  books: [
+    {
+      type: ObjectId,
+      ref: "Book",
+    },
+  ],
+});
+
+// Creating a Data Model
+const Genre = model("Genre", genreSchema);
+
+// Exporting the Data Model
+module.exports = Genre;
