5f581e3362c7b1af8e5312782db8fcecfadaaf1a
Himanshu Khaitan <himanshukhaitan108@gmail.com>
2022-11-30 01:57:29 +0530

    Logging Enabled

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 // Import Statements
 const express = require("express");
 const http = require("http");
+const bodyParser = require("body-parser");
+const logging = require("./middlewares/logging");
 
 // Creating Express App
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(logging);
+
+// PARSE application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// PARSE application/json
+app.use(bodyParser.json());
 
 // Routes
 app.use("/api/add", require("./routes/add/Index"));
