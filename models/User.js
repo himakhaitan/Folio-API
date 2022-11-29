4cc570fbf9b4bdf81ac0dbefffe4463d43f4087a
Himanshu Khaitan <himanshukhaitan108@gmail.com>
2022-11-29 21:47:47 +0530

    User Model Definition 💁

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose");
+
+// Defining Data Types
+let ObjectId = Schema.Types.ObjectId;
+
+// Creating a Schema
+const userSchema = new Schema({
+  // First Name of the User
+  firstName: {
+    type: String,
+    required: true,
+  },
+  // Last Name of the User
+  lastName: {
+    type: String,
+    required: true,
+  },
+  // Email of the User
+  email: {
+    type: String,
+    required: true,
+  },
+  // Issues of the User
+  issues: [
+    {
+      book: {
+        type: ObjectId,
+        ref: "Book",
+      },
+      issuedAt: Date,
+    },
+  ],
+  // Dues of the User
+  dues: {
+    type: Number,
+    default: 0,
+  },
+});
+
+// Creating Data Model
+const User = model("User", userSchema);
+
+module.exports = User;
