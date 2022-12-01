a6c67b348e13a26ec4aaf08da34672e5a6126a84
Himanshu Khaitan <himanshukhaitan108@gmail.com>
2022-12-01 22:33:28 +0530

    Genre fetch routes added!

--- routes/data/Index.js
+++ routes/data/Index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // Including Add Routes
 // router.use("/", require("./book"));
 // router.use("/", require("./author"));
-// router.use("/", require("./genre"));
+router.use("/", require("./genre"));
 router.use("/", require("./user"));
 
 
---
+++ routes/data/genre.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const router = express.Router();
+
+const Genre = require("../../models/Genre");
+
+// Fetching Books of a Genre with Object ID
+router.get("/genre/id/:id", async (req, res) => {
+  // Getting the User ID
+  let id = req.params.id;
+
+  // Checking the Object ID
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({
+      message: "Invalid Object ID",
+    });
+  }
+
+  // Fetching the Genre
+  try {
+    const genre = await Genre.findById(id);
+    if (!genre) {
+      // If Genre not found
+      return res.status(404).json({
+        message: "Genre not found",
+      });
+    }
+    // Sending Response
+    return res.status(200).json({
+      genre: {
+        id: genre._id,
+        name: genre.name,
+        slug: genre.slug,
+        books: genre.books,
+      },
+      success: true,
+      message: "Genre found",
+    });
+  } catch (err) {
+    // Dealing with Errors
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+
+// Fetching Books of a Genre with Slug
+router.get("/genre/:slug", async (req, res) => {
+  // Getting the Slug
+  let slug = req.params.slug;
+
+  // Cheking validity of Slug
+  if (!slug || slug.length == 0) {
+    return res.status(400).json({
+      message: "Invalid Slug",
+    });
+  }
+
+  // Fetching the Genre
+  try {
+    const genre = await Genre.findOne({
+      slug: slug,
+    });
+    if (!genre) {
+      // If Genre not found
+      return res.status(404).json({
+        message: "Genre not found",
+      });
+    }
+    // Sending Response
+    return res.status(200).json({
+      genre: {
+        id: genre._id,
+        name: genre.name,
+        slug: genre.slug,
+        books: genre.books,
+      },
+      success: true,
+      message: "Genre found",
+    });
+  } catch (err) {
+    // Dealing with Errors
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+
+module.exports = router;
